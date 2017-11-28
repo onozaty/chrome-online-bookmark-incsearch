3fcc77a13913802234d6a768723e490a43e9993b
onozaty <onozaty@gmail.com>
2017-11-29 02:14:17 +0900

    Pinboard用のブックマーク取得処理実装。

---
+++ app/ui/pinboard.js
@@ -0,0 +1,55 @@
+class PinboardService {
+
+  createEditUrl(bookmark) {
+    return 'https://pinboard.in/add?next=same&url=' + encodeURIComponent(bookmark.url);
+  }
+
+  newLoader() {
+    return new PinboardLoader();
+  }
+}
+
+class PinboardLoader {
+
+  load() {
+    return new Promise((resolve, reject) => {
+      $.ajax(
+        'https://api.pinboard.in/v1/posts/all',
+        {
+          dataType: 'xml'
+        })
+        .done((xml) => {
+          
+          const $xml = $(xml);
+          const userId = $xml.find('posts').attr('user');
+          const bookmarks = $xml.find('post').map((index, element) => {
+            const $element = $(element);
+
+            const bookmark = {};
+            bookmark.id = index;
+            bookmark.url = $element.attr('href');
+            bookmark.title = $element.attr('description');
+            bookmark.description = $element.attr('extended');
+            bookmark.tags = $element.attr('tag').split(' ').map((item) => '[' + item + ']').join(' ');
+            bookmark.time = $element.attr('time');
+            bookmark.searchableText = [
+              bookmark.title,
+              bookmark.description,
+              bookmark.tags
+            ].join("\n").toLowerCase();
+
+            return bookmark;
+          });
+
+          resolve({
+            userId: userId,
+            bookmarks: bookmarks
+          });
+        })
+        .fail((jqXHR, textStatus, errorThrown) => {
+          reject(`${jqXHR.status}(${jqXHR.statusText}))`);
+        });
+    });
+  }
+
+}
